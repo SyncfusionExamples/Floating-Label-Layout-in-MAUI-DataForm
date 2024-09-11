fdca49355cf21bd71257f5e9a27460f3cad8ae6a
snehakPV <sneha.sf4154@gmail.com>
2024-09-11 15:17:15 +0530

    Updated the sample and read me file

---
+++ DataFormSample/DataFormSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataFormSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
